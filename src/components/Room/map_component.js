ebd1a352181543dc151ce4711b944127674848da
John Huynh <huynh.j93@gmail.com>
2017-03-10 08:33:35 -0800

    [add]-Link to home page after end game and leave game

--- src/components/Room/map_component.js
+++ src/components/Room/map_component.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const  lat = localStorage.getItem('lat');
 const  lng = localStorage.getItem('lng');
@@ -18,8 +19,8 @@ render() {
 	return (
 <div>
 	<iframe id="map" className="center-block" src={map}></iframe>
-		<button className="center-block">end game</button>
-		<button className="center-block">leave game</button>
+		<button className="center-block"><Link to='/'>end game</Link></button>
+		<button className="center-block"><Link to='/'>leave game</Link></button>
  </div>
     );
   }
